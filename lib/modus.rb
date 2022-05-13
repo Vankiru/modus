1c384701631f8b8b54ad19300b4cbcd4b3f7427d
Julia Egorova <j.e.egorova@gmail.com>
2022-05-13 16:49:34 +0400

    Init commit

---
+++ lib/modus.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "modus/version"
+
+module Modus
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/modus_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Modus do
+  it "has a version number" do
+    expect(Modus::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
